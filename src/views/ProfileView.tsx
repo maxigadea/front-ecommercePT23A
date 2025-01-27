f01c86cf7840c67f78d097ec898a56ac6f624ada
Maxi Gadea <maxigadea1998@gmail.com>
2025-01-27 20:33:23 -0300

    test

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -9,7 +9,7 @@ const ProfileView = () => {
         <h1>Bienvenido a tu perfil</h1>
         <h4>Tu correo: {userData?.user.email}</h4>
         <h4>Tu dirección: {userData?.user.address}</h4>
-        <h4>Tu número: {userData?.user.phone}</h4>
+        <h4>Tu número:    {userData?.user.phone}</h4>
     </div>
   )
 }
